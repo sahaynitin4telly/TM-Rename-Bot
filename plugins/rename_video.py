7e217e1a78b109c4a4019ce006bc9261c484d495
TMAdminz <71258154+TMAdminz@users.noreply.github.com>
2020-11-18 09:42:56 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -121,7 +121,7 @@ async def rename_video(bot, update):
                 video=new_file_name,
                 duration=duration,
                 thumb=thumb_image_path,
-                caption=f"<b>{file_name} \n\nShare and Support\n\n@SonyPictures_Tamil</b>",
+                caption=f"<b>{file_name} \n\nShare and Support\n\n<a href='https://t.me/joinchat/AAAAAFIN2ibGFWeYBNj2Xg'>@TM_Moviez</a></b>",
                 # reply_markup=reply_markup,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,

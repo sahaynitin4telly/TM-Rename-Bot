66afa529c2198088e73773849f84afe4d1ee3c1e
TMAdminz <71258154+TMAdminz@users.noreply.github.com>
2021-07-08 15:57:57 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -16,7 +16,7 @@ Type /help for more details."""
     UPLOAD_START_VIDEO = "**Uploading Started️**"
     UPLOAD_START = "**Uploading Started️**"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using  [Join](https://t.me/joinchat/AAAAAFIN2ibGFWeYBNj2Xg). Please /donate for bot alive**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using  [Join](https://t.me/TM_Moviez). Please /donate for bot alive**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/MonkAno'>AI_RenameBot</a>"
@@ -24,7 +24,7 @@ Type /help for more details."""
     DEL_ETED_CUSTOM_THUMB_NAIL = "🤔 Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "🤨 Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "👹 Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "<b>\nShare and Support</b> \n\n<a href='https://t.me/joinchat/AAAAAFIN2ibGFWeYBNj2Xg'><b>TM_Moviez</b></a>"
+    CUSTOM_CAPTION_UL_FILE = "<b>\nShare and Support</b> \n\n<a href='https://t.me/TM_Moviez'><b>TM_Moviez</b></a>"
     NO_CUSTOM_THUMB_NAIL_FOUND = "😠 No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """Hai <b><i>{}</i></b>, 
@@ -41,7 +41,7 @@ I am <a href='https://t.me/TM_RenameBot'><b>TM Rename Bot</b></a> by <a href='ht
 
 --------
 
-Movie Channel : <a href='https://t.me/joinchat/AAAAAFIN2ibGFWeYBNj2Xg'><b>TM_Moviez</b></a>"""
+Movie Channel : <a href='https://t.me/TM_Moviez'><b>TM_Moviez</b></a>"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "🙆 Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
     REPLY_TO_DOC_FOR_RENAME_VIDEO = "🙋 Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
